5255bc1509bb828668c476b187647d20290c2329
duyduc1108@gmail.com <duyduc1108@gmail.com@c1f3c89d-4cd5-45d6-dba9-e6e7e713d681>
2015-01-11 19:05:48 +0000

    DD - Modification doGet du servlet Authentification -Amelioration des pages VmProf

--- trunk/Cloud-Amra/src/java/PAAS/Servlets/VmProfServlet.java
+++ trunk/Cloud-Amra/src/java/PAAS/Servlets/VmProfServlet.java
@@ -10,6 +10,7 @@ import IAAS.Iaas;
 import PAAS.Models.Groupe;
 import PAAS.Models.User;
 import PAAS.Models.UserGroupe;
+import PAAS.Models.Vm;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -26,73 +27,93 @@ import net.elbandi.pve2api.data.Container;
 public class VmProfServlet extends HttpServlet{
     
     private static final String VUE_VM_PROF = "/WEB-INF/Professor/vmProf.jsp";
+    private static final String VUE_VM_PROF_MODIFY = "/WEB-INF/Professor/vmProfModify.jsp";
     private static final String ATTR_LISTE_TEMPLATE="ListeTemplate";  
     private static final String ATTR_LISTE_GROUPE="ListeGroupe";
+    private static final String ATTR_LISTE_VM="ListeVM";
     
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         Form form =  new Form ();
         HttpSession session = request.getSession();
-         
-        session.setAttribute(ATTR_LISTE_TEMPLATE, form.getListTemplate());
-        session.setAttribute(ATTR_LISTE_GROUPE, form.getListGroupe());
-        this.getServletContext().getRequestDispatcher(VUE_VM_PROF).forward(request, response);
+        User prof = (User) session.getAttribute("sessionUser"); 
+        if (request.getParameter("actionChange") == null){
+                   
+            session.setAttribute(ATTR_LISTE_TEMPLATE, form.getListTemplate());
+            session.setAttribute(ATTR_LISTE_GROUPE, form.getListGroupe());
+            session.setAttribute(ATTR_LISTE_VM, form.getListVmByProf(prof));
+
+            this.getServletContext().getRequestDispatcher(VUE_VM_PROF).forward(request, response); 
+        }
+        else{
+            this.getServletContext().getRequestDispatcher(VUE_VM_PROF_MODIFY).forward(request, response); 
+        }
+
 
     }
     
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        HttpSession session = request.getSession();
-        System.out.println("RAM="+request.getParameter("ram"));
-        System.out.println("CPUS="+request.getParameter("cpus"));
-        System.out.println("DISK="+request.getParameter("disk"));
-        System.out.println("TEMPLATE="+request.getParameter("template"));
-        System.out.println("GROUPE="+request.getParameter("groupe"));
-        System.out.println("MDP="+request.getParameter("passwordDefault"));
-        
-        Iaas ias = new Iaas();
+        if (!request.getParameter("actionAdd").isEmpty()){
+                    
+            HttpSession session = request.getSession();
+            System.out.println("RAM="+request.getParameter("ram"));
+            System.out.println("CPUS="+request.getParameter("cpus"));
+            System.out.println("DISK="+request.getParameter("disk"));
+            System.out.println("TEMPLATE="+request.getParameter("template"));
+            System.out.println("GROUPE="+request.getParameter("groupe"));
+            System.out.println("MDP="+request.getParameter("passwordDefault"));
 
-        //un container - un etudiant
-        /*Container cont = new Container(request.getParameter("template"),
-        "101",request.getParameter("cpus"),request.getParameter("disk")
-                ,"test44",request.getParameter("ram"),"test");*/
-        //ias.creerContainer(cont, 18);
-        
-        
-        Form form = new Form();
-        //Recuperation du groupe avec sa libelle
-        Groupe groupe = form.getGroupeByLibelle(request.getParameter("groupe"));
-        //Recuperation la liste des etudiants concernant le groupe
-        List<UserGroupe> students = form.getListStudentByGroupe(groupe);
-        System.out.println("Creation des containers");
-        boolean result=false;
-        
-        for(UserGroupe user : students){
-            //Recuperation l'etudiant du groupe
-            User student = form.getStudentInGroupe(user);
-            System.out.println("Nom=" + student.getNom());
-            
-            //Ajout du VM dans la BD
-            result = form.addVM(student, groupe,request.getParameter("passwordDefault")+student.getNom());
-            System.out.println("Add VM OK for " + student.getNom());
-            
-            //Si l'ajout dans la BD est OK, on cree un container pour cette etudiant
-            if (result){
-                System.out.println("Creation CONTAINER");
-                //Creation du VM pour un etudiant
-                Container container = new Container(request.getParameter("template"),form.getVmByStudentAndGroupe(student, groupe).getId().toString(),
-                request.getParameter("cpus"),request.getParameter("disk"),"test",request.getParameter("cpus"),request.getParameter("disk"),
-                request.getParameter("passwordDefault")+user.getId());
-                
-                //Ajout du container
-                ias.creerContainer(container, 1);
-                System.out.println("Creation OK pour "+ user.getId());
+            Iaas ias = new Iaas();
+
+            //un container - un etudiant
+            /*Container cont = new Container(request.getParameter("template"),
+            "101",request.getParameter("cpus"),request.getParameter("disk")
+                    ,"test44",request.getParameter("ram"),"test");*/
+            //ias.creerContainer(cont, 18);
+            User prof = (User) session.getAttribute("sessionUser");
+
+            Form form = new Form();
+            //Recuperation du groupe avec sa libelle
+            Groupe groupe = form.getGroupeByLibelle(request.getParameter("groupe"));
+            //Recuperation la liste des etudiants concernant le groupe
+            List<UserGroupe> students = form.getListStudentByGroupe(groupe);
+            System.out.println("Creation des containers");
+            boolean result=false;
+
+            for(UserGroupe user : students){
+                //Recuperation l'etudiant du groupe
+                User student = form.getStudentInGroupe(user);
+                System.out.println("Nom=" + student.getNom());
+
+                //Ajout du VM dans la BD
+                result = form.addVM(student, groupe,request.getParameter("passwordDefault")+student.getNom(),prof.getId().intValue());
+                System.out.println("Add VM OK for " + student.getNom());
+
+                //Si l'ajout dans la BD est OK, on cree un container pour cette etudiant
+                if (result){
+                    System.out.println("Creation CONTAINER");
+                    //Creation du VM pour un etudiant
+                    Container container = new Container(request.getParameter("template"),form.getVmByStudentAndGroupe(student, groupe).getId().toString(),
+                    request.getParameter("cpus"),request.getParameter("disk"),"test",request.getParameter("cpus"),request.getParameter("disk"),
+                    request.getParameter("passwordDefault")+user.getId());
+
+                    //Ajout du container
+                    ias.creerContainer(container, 1);
+                    System.out.println("Creation OK pour "+ user.getId());
+                }
+                else{
+                    System.out.println("Problem avec l'ajout à la BD");
+                }
             }
-            else{
-                System.out.println("Problem avec l'ajout à la BD");
-            }
+            System.out.println("DONE");
         }
-        System.out.println("DONE");
+        else if (!request.getParameter("actionUpdate").isEmpty()){
+            
+        }
+        else{
+            
+        }
+
         
        this.getServletContext().getRequestDispatcher(VUE_VM_PROF).forward(request, response);
         
---
+++ trunk/Cloud-Amra/web/WEB-INF/Professor/vmProfModify.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : vmProfModify
+    Created on : Jan 11, 2015, 7:30:14 PM
+    Author     : Duy Duc
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
